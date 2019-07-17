87d12e1b5ae515e6ad7a49181f4910e75b5b4a84
Krzysztof Branecki <krzysztof@branecki.net>
2019-07-17 18:07:47 +0200

    Country search engine

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const url = "https://restcountries.eu/rest/v2/name/",
+  countriesList = document.getElementById("countries");
+
+document.getElementById("search").addEventListener("click", searchCountries);
+
+function prependChild(parentEle, newFirstChildEle) {
+  parentEle.insertBefore(newFirstChildEle, parentEle.firstChild);
+}
+function searchCountries() {
+  let countryName = document.getElementById("country-name").value;
+  if (!countryName.length) countryName = "Poland";
+
+  fetch(url + countryName)
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+  countriesList.innerHTML = "";
+  console.log(resp);
+  resp.forEach(function({ name, capital, flag }) {
+    let liEl = document.createElement("li");
+    let spanEl = document.createElement("span");
+    let imgEl = document.createElement("img");
+
+    imgEl.src = flag;
+
+    liEl.innerText = name;
+    liEl.insertAdjacentElement("afterbegin", imgEl);
+    spanEl.innerText = " Capital city: " + capital;
+
+    liEl.appendChild(spanEl);
+    countriesList.appendChild(liEl);
+  });
+}
